9927359ec43288e992f5f6a564faef7348a860d7
MadhavSoliya <madhavsoliya1@gmail.com>
2015-01-28 17:37:20 +0530

    MongoWebService using node.js

---
+++ mongowebservice/routes/service.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+var xml = require('xml');
+
+var mongoose = require('mongoose');
+var Todo = require('../models/model.js');
+
+/* GET /todos listing. */
+router.get('/', function(req, res, next) {
+  Todo.find(function (err, todos) {
+    if (err) return next(err);
+    res.json(todos);
+  });
+});
+
+//find by id
+router.get('/:id', function(req, res, next) {
+  Todo.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//find by username
+router.get('/users/:username', function (req, res,next) {
+  Todo.find({'username':req.params.username}, function (err, todos) {
+    if (err) return next(err);
+	
+	var jsonString = JSON.stringify(todos);
+	//res.json(todos);
+	res.send('{'+'"result":'+jsonString +'}');
+  });
+});
+
+
+router.route('/users').post(function(req, res) {
+  var movie = new model(req.body);
+ 
+  movie.save(function(err) {
+    if (err) {
+      return res.send(err);
+    }
+ 
+    res.send({ message: 'Movie Added' });
+  });
+});
+
+module.exports = router;
